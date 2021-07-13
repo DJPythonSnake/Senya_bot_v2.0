8c372dda6ccbc681aa7992c21cabfb2df1dcdeef
DJPytohnSnake <romaakimenko10@gmail.com>
2021-07-13 20:43:50 +0300

    refactor code

--- main.py
+++ main.py
@@ -1,39 +1,28 @@
 #imports
 import os
-import time
 import webbrowser
 
 
-def say(text_command):
-    
-    if text_command == 'погуглить':
-        os.system('clear')
-        url = input('google: ')
-        webbrowser.open_new('google.com/search?q=' + url)
+def google_search():
+    os.system('clear')
+    url = input('google: ')
+    webbrowser.open_new('google.com/search?q=' + url)
 
-    elif text_command == 'запустить команду':
-        os.system('clear')
-        while True:
-            cmd = input('>>> ')
-            os.system(cmd)
+def cmd_run():
+    os.system('clear')
+    while True:
+        cmd = input('>>> ')
+        os.system(cmd)
 
-            if cmd == 'exit':
-                break
-    
-    elif text_command == 'посчитать':
-        os.system('clear')
-        print('calculator:')
+        if cmd == 'exit':
+            break
 
-        culc = eval(input())
-        print(culc)
-        
-    elif text_command == 'выйти':
-        print("Пока!")
-        quit()
-    else:
-        os.system('clear')
-        print('Я не понимаю')
-        time.sleep(1)
+def num_count():
+    os.system('clear')
+    print('calculator:')
+
+    culc = eval(input())
+    print(culc)
 
 
 
@@ -41,10 +30,20 @@ def say(text_command):
 
 
 while True:
-    funcs = ['Погуглить', 'Запустить команду', 'Посчитать']
+    funcs = ['погуглить', 'запустить команду', 'посчитать']
     for i in funcs:
         print('-------------------------------')
         print(i)
 
     text_command = input().lower()
-    say(text_command)
+    
+    if text_command == funcs[0]:
+        google_search()
+    elif text_command == funcs[1]:
+        cmd_run()
+    elif text_command == funcs[2]:
+        num_count()
+    elif text_command == 'выйти':
+        break
+    else:
+        print("Я не понимаю") 
